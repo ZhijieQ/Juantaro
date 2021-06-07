6500b9c7357f43c8b083e28e46f3652cfb1429f6
Kuroha 96 <>
2021-06-07 22:07:39 +0000

    pvto

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ client.on("message", async msg => {
 	await commands.executeCmd(msg, args)
 
 	if (msg.content === "Hello"){
-		util.send(msg, `Hello ${msg.author}!`)
+		util.send(msg, `Hello pvto ${msg.author}!`)
 	}
 
 	/*if (msg.content.toLowerCase().startsWith("test")){
